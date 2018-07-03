dccef5ee4fcc518961edfac0efd4b89ac8d8fa2f
lileihappy123 <448117472@qq.com>
2018-07-03 18:28:54 +0800

    change the view page to jsp page

--- src/main/java/com/spring/boot/springBootDemo/SpringBootDemoApplication.java
+++ src/main/java/com/spring/boot/springBootDemo/SpringBootDemoApplication.java
@@ -2,11 +2,14 @@ package com.spring.boot.springBootDemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 //@EnableDiscoveryClient
 @SpringBootApplication
-public class SpringBootDemoApplication {
+@ServletComponentScan
+public class SpringBootDemoApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootDemoApplication.class, args);
